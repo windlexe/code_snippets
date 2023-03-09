8dcefdc3a1638da51b56edeaa0f248af7c6b60f9
windlexe <127382962+windlexe@users.noreply.github.com>
2023-03-09 22:13:39 +0800

    lua-delegate

---
+++ lua/delegate.lua
@@ -0,0 +1,25 @@
+delegate = class ()
+
+function delegate:__ctor()
+end
+
+function delegate:__call(...)
+	for i, fn in ipairs(self.fn_list) do
+        fn(...)
+    end
+end
+
+function delegate:add(fn)
+    tinsert(self.fn_list, fn)
+end
+
+function test1()
+	local a = delegate()
+    local fa = function (...)
+        print('fa', ...)
+    end
+    a += fa
+    a()
+end
+
+
